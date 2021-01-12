3acdb1c21d0782fc0630e2a6f2e9572f88663892
Jozi <koder@creativekode.com>
2021-01-11 19:32:32 -0500

    updated host check to staging url

--- javascripts/discourse/initializers/config.js
+++ javascripts/discourse/initializers/config.js
@@ -1,9 +1,9 @@
 const ThemeConfig = {
     development: {
-        lambdaUrl: (message)=>{return `https://qqz32mg1oi.execute-api.us-east-1.amazonaws.com/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
+        lambdaUrl: (message)=>{return `https://qqz32mg1oi.execute-api.us-east-1.amazonaws.com/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`; }
     },
-    digitalOcean: {
-        lambdaUrl: (message)=>{return `https://web33.ngrok.io/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
+    localhost: {
+        lambdaUrl: (message)=>{return `https://maker.ngrok.io/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
     },
     production: {
         lambdaUrl: (message)=>{return `https://3nsc1491ed.execute-api.us-east-1.amazonaws.com/dev/discourse?username=${message.username}&address=${message.address}&signature=${message.signature}`;}
